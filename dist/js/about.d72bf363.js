673e0d3f0d83ff80d0cbf31f1d814542959eb837
Jiafu <jiafu20001003@gmail.com>
2023-12-23 12:43:37 +0800

    add dist folder

---
+++ dist/js/about.d72bf363.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_final"]=self["webpackChunkvue_final"]||[]).push([[443],{833:function(n,u,a){a.r(u),a.d(u,{default:function(){return o}});var e=a(3396);const t={class:"about"},r=(0,e._)("h1",null,"This is an about page",-1),s=[r];function i(n,u){return(0,e.wg)(),(0,e.iD)("div",t,s)}var c=a(89);const f={},l=(0,c.Z)(f,[["render",i]]);var o=l}}]);
+//# sourceMappingURL=about.d72bf363.js.map
---
+++ src/views/Login.vue
@@ -0,0 +1,67 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<!-- eslint-disable vuejs-accessibility/label-has-for -->
+<!-- eslint-disable vuejs-accessibility/no-autofocus -->
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div class="container mt-5">
+        <form action="" class="row justify-content-center" @submit.prevent="singIn">
+            <div class="col-md-6">
+                <h1 class="h3 mb-3 font-weight-normal">
+                    請先登入
+                </h1>
+                <!-- required進行email驗證 -->
+                <!-- autofocus進入畫面不使用滑鼠即可focus在輸入框 -->
+                <div class="mb-2">
+                    <label for="inputEmail" class="sr-only">Email address</label>
+                    <input
+                        type="email"
+                        id="inputEmail"
+                        class="form-control"
+                        placeholder="Email address"
+                        required
+                        autofocus
+                        v-model="user.username"
+                    />
+                </div>
+                <div class="mb-2">
+                    <label for="inputPassword" class="sr-only">Password</label>
+                    <input
+                        type="password"
+                        id="inputPassword"
+                        class="form-control"
+                        placeholder="Password"
+                        required
+                        v-model="user.password"
+                    />
+                    </div>
+                    <div class="text-end mt-4">
+                    <button class="btn btn-lg btn-primary btn-block" type="submit">登入</button>
+                    </div>
+            </div>
+        </form>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        username: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    singIn() {
+      // 透過$讀出.env環境變數裡站點的位置，連接後方admin/signin的API路徑
+      const api = `${process.env.VUE_APP_API}admin/signin`;
+      // 透過axios中的this.$http取得方法，使用POST(包含API,夾帶的資料)
+      // promise使用.then方法進行串接
+      this.$http.post(api, this.user)
+        .then((res) => {
+          console.log(res);
+        });
+    },
+  },
+};
+</script>
