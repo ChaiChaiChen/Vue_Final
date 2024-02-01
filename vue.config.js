7f9b0776026d8c97ae9d58fd390cae3a96cf531c
Jiafu <jiafu20001003@gmail.com>
2024-02-01 12:05:02 +0800

    檔案名稱修改

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/Soffa/dist',
+  publicPath: '/Vue_Final/dist',
 });
